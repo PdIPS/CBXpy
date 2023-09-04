c0e1e1e7ab6919648466d5e16096a80a73d45c44
Tim Roith <44805883+TimRoith@users.noreply.github.com>
2023-09-04 13:19:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
                 "Operating System :: OS Independent"],
     install_requires=[  'numpy', 
                         'scipy', 
-                        'sklearn', 
+                        'scikit-learn', 
                         'matplotlib'],
     python_requires='>=3.6',
 )
