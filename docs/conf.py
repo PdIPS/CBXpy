e6756815a5f133c029b857ffd05e2707f8435056
Tim Roith <tim.roith@fau.de>
2023-11-21 19:37:01 +0100

    additions to doc

--- docs/conf.py
+++ docs/conf.py
@@ -83,12 +83,12 @@ html_theme_options = {
       {
          "rel": "icon",
          "sizes": "16x16",
-         "href": "cbx_py32x32.png",
+         "href": "cbx_py32x32.ico",
       },
       {
          "rel": "icon",
          "sizes": "32x32",
-         "href": "cbx_py32x32.png",
+         "href": "cbx_py32x32.ico",
       },
    ]
 }
