79891172505f2cf5f00594394e4a7c02a0b3ef9c
Roith <roithtim@desy.de>
2024-05-03 10:58:45 +0200

    added torch option

--- cbx/utils/__init__.py
+++ cbx/utils/__init__.py
@@ -1,5 +1,6 @@
 from .particle_init import init_particles
 from . import resampling
+from . import torch_utils
 
 
-__all__ = ['init_particles', 'resampling']
+__all__ = ['init_particles', 'resampling', 'torch_utils']
--- cbx/utils/torch_utils.py
+++ cbx/utils/torch_utils.py
@@ -1,49 +1,72 @@
 from collections import OrderedDict
-try:
+from functools import wraps
+from ..scheduler import bisection_solve, eff_sample_size_gap
+import numpy as np
+
+try: # try torch import
     import torch
     from torch import logsumexp
     from torch.func import functional_call, stack_module_state, vmap
 except ImportError:
-    _has_numpy = False
+    _has_torch = False
 else:
-    _has_numpy = True
+    _has_torch = True
 
-from cbx.scheduler import bisection_solve, eff_sample_size_gap
-import numpy as np
 
+#%%
+def requires_torch(f):
+    @wraps(f)
+    def torch_decorated(*args, **kwargs):
+        if not _has_torch:
+            raise ImportError('The requested function: ' + f.__name__ + 'requires ' + 
+                              'torch, but torch import failed! Either install torch manually ' +
+                              'or install cbx with torch option: pip install cbx[torch]')
+        return f(*args, **kwargs)
+
+    return torch_decorated
+
+@requires_torch
 def norm_torch(x, axis, **kwargs):
     return torch.linalg.norm(x, dim=axis, **kwargs)  
 
+@requires_torch
 def compute_consensus_torch(energy, x, alpha):
     weights = - alpha * energy
     coeffs = torch.exp(weights - logsumexp(weights, dim=(-1,), keepdims=True))[...,None]
     return (x * coeffs).sum(axis=-2, keepdims=True), energy.cpu().numpy()
 
+@requires_torch
 def compute_polar_consensus_torch(energy, x, neg_log_eval, alpha = 1., kernel_factor = 1.):
     weights = -kernel_factor * neg_log_eval - alpha * energy[:,None,:]
     coeffs = torch.exp(weights - torch.logsumexp(weights, dim=(-1,), keepdims=True))[...,None]
     c = torch.sum(x[:,None,...] * coeffs, axis=-2)
     return c, energy.cpu().numpy()
 
+@requires_torch
 def normal_torch(device):
     def _normal_torch(mean, std, size):
         return torch.normal(mean, std, size).to(device)
     return _normal_torch
 
+@requires_torch
 def eval_model(x, model, w, pprop):
     params = {p: w[pprop[p][-2]:pprop[p][-1]].view(pprop[p][0]) for p in pprop}
     return functional_call(model, (params, {}), x)
 
+@requires_torch
 def eval_models(x, model, w, pprop):
     return vmap(eval_model, (None, None, 0, None))(x, model, w, pprop)
 
+@requires_torch
 def eval_loss(x, y, loss_fct, model, w, pprop):
     with torch.no_grad():
         return loss_fct(eval_model(x, model, w, pprop), y)
-    
+
+@requires_torch
 def eval_losses(x, y, loss_fct, model, w, pprop):
     return vmap(eval_loss, (None, None, None, None, 0, None))(x, y, loss_fct, model, w, pprop)
 
+@requires_torch
 def eval_acc(model, w, pprop, loader):
     res = 0
     num_img = 0
@@ -54,11 +77,13 @@ def eval_acc(model, w, pprop, loader):
         num_img += x.shape[0]
     return res/num_img
 
+@requires_torch
 def flatten_parameters(models, pnames):
     params, buffers = stack_module_state(models)
     N = list(params.values())[-1].shape[0]
     return torch.concatenate([params[pname].view(N,-1).detach() for pname in pnames], dim=-1)
 
+@requires_torch
 def get_param_properties(models, pnames=None):
     params, buffers = stack_module_state(models)
     pnames = pnames if pnames is not None else params.keys()
@@ -70,7 +95,7 @@ def get_param_properties(models, pnames=None):
         pprop[p] = (params[p][0,...].shape, a, a + params[p][0,...].numel())
     return pprop
 
-
+@requires_torch
 class effective_sample_size:
     def __init__(self, name = 'alpha', eta=.5, maximum=1e5, minimum=1e-5, solve_max_it = 15):
         self.name = name
