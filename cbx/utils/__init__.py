b8a8c5e56526aef3236a6a849244bb6cd79b275c
Tim Roith <tim.roith@tum.de>
2026-07-14 15:33:56 +0200

    fix torch importing and bump version

--- cbx/utils/__init__.py
+++ cbx/utils/__init__.py
@@ -1,6 +1,14 @@
+import importlib
+
 from .particle_init import init_particles
 from . import resampling
-from . import torch_utils
-
 
 __all__ = ['init_particles', 'resampling', 'torch_utils']
+
+
+def __getattr__(name):
+    if name == 'torch_utils':
+        module = importlib.import_module('.torch_utils', __name__)
+        globals()['torch_utils'] = module
+        return module
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
